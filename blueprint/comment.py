0041e397361e152d4712b0d32bb4acca5bfab5cd
y-matsuki <yuto.matsuki@recochoku.co.jp>
2015-09-16 20:28:35 +0900

    add rating

--- blueprint/comment.py
+++ blueprint/comment.py
@@ -10,17 +10,17 @@ from common import db
 bp_comment = Blueprint('bp_comment', __name__,
                     template_folder='templates')
 
-@bp_comment.route('/<event_id>/<owner>')
-def show_comments(event_id=None,owner=None):
-    comments = db.comments.find({"event_id":event_id, "owner":owner})
-    return render_template('/comments.html', comments=list(comments))
 
-@bp_comment.route('/<event_id>/<owner>/<username>')
-def show_comment(event_id=None,owner=None,username=None):
-    owner = session['username']
-    rating = db.ratings.find_one({"owner":owner,"event_id":event_id,"username":username});
+@bp_comment.route('/<event_id>/<presenter>')
+def show_comment(event_id=None,presenter=None):
+    listener = session['username']
+    rating = db.ratings.find_one({
+        "event_id":event_id, "presenter":presenter, "listener":listener
+    });
     axes = []
-    if rating == None:
+    if rating:
+        axes = rating['axes']
+    else:
         axes = [
             {"name":"theme",       "jp_name": u"お題", "value":3},
             {"name":"authority",   "jp_name": u"説得力", "value":3},
@@ -29,38 +29,37 @@ def show_comment(event_id=None,owner=None,username=None):
             {"name":"time",        "jp_name": u"時間配分", "value":3},
             {"name":"deep",        "jp_name": u"深掘り力", "value":3}
         ]
-    else:
-        axes = rating['axes']
-    print(axes)
-    return render_template('comment.html', event_id=event_id, username=username,
+    return render_template('comment.html', event_id=event_id, presenter=presenter,
                                            axes=axes)
 
 
-@bp_comment.route('/<event_id>/<username>', methods=['POST'])
-def post_comment(event_id=None,username=None):
-    if 'username' in session and username != session['username']:
+@bp_comment.route('/<event_id>/<presenter>', methods=['POST'])
+def post_comment(event_id=None,presenter=None):
+    if 'username' in session and presenter != session['username']:
         axes = [
             {"name":"theme",       "jp_name": u"お題", "value": int(request.form['theme'])},
             {"name":"authority",   "jp_name": u"説得力", "value": int(request.form['authority'])},
             {"name":"originality", "jp_name": u"独創性", "value": int(request.form['originality'])},
             {"name":"logicality",  "jp_name": u"論理性", "value": int(request.form['logicality'])},
             {"name":"time",        "jp_name": u"時間配分", "value": int(request.form['time'])},
-            {"name":"deep",        "jp_name": u"深掘り力", "value": int(request.form['deeper'])}
+            {"name":"deep",        "jp_name": u"深掘り力", "value": int(request.form['deep'])}
         ]
+        listener = session['username']
         rating = {
             "event_id": event_id,
-            "username": username,
+            "presenter": presenter,
             "axes": axes,
-            "owner": session['username']
+            "listener": listener
         }
-        db.ratings.update_one({"event_id":event_id,"username":username},
-                              {"$set": rating}, upsert=True)
+        db.ratings.update_one({
+            "event_id":event_id, "presenter":presenter, "listener":listener},
+            {"$set": rating}, upsert=True)
         if request.form.has_key('good'):
             comment = {
                 "type": "good",
                 "event_id": event_id,
                 "comment": request.form['good'],
-                "owner": username
+                "presenter": presenter
             }
             db.comments.insert(comment);
         if request.form.has_key('bad'):
@@ -68,7 +67,55 @@ def post_comment(event_id=None,username=None):
                 "type": "bad",
                 "event_id": event_id,
                 "comment": request.form['bad'],
-                "owner": username
+                "presenter": presenter
             }
             db.comments.insert(comment);
     return redirect('/home')
+
+
+@bp_comment.route('/<event_id>')
+def show_comments(event_id=None):
+    presenter = session['username']
+    event = db.events.find_one({"event_id":event_id})
+    theme = {}
+    for item in event['themes']:
+        if presenter == item['username']:
+            theme = item
+    comments = db.comments.find({"event_id":event_id, "presenter":presenter})
+    ratings = db.ratings.find({"event_id":event_id, "presenter":presenter})
+    return render_template('/comments.html', comments=list(comments),
+                                             theme=theme, rating=len(list(ratings)),
+                                             event_id=event_id,presenter=presenter)
+
+
+@bp_comment.route('/api/<event_id>/<presenter>')
+def get_rating(event_id=None,presenter=None):
+    ratings = list(db.ratings.find({"event_id":event_id, "presenter":presenter}))
+    size = len(ratings) * 1.0
+    sum_theme, sum_authority, sum_originality, sum_logicality, sum_time, sum_deep = 0, 0, 0, 0, 0, 0
+    for rating in ratings:
+        for axe in rating['axes']:
+            if axe['name'] == 'theme':
+                sum_theme += axe['value']
+            elif axe['name'] == 'authority':
+                sum_authority += axe['value']
+            elif axe['name'] == 'originality':
+                sum_originality += axe['value']
+            elif axe['name'] == 'logicality':
+                sum_logicality += axe['value']
+            elif axe['name'] == 'time':
+                sum_time += axe['value']
+            elif axe['name'] == 'deep':
+                sum_deep += axe['value']
+    item = {
+        "labels": [u"お題", u"説得力", u"独創性", u"論理性", u"時間配分", u"深掘り力"],
+        "data": [
+            sum_theme / size,
+            sum_authority / size,
+            sum_originality / size,
+            sum_logicality / size,
+            sum_time / size,
+            sum_deep / size
+        ]
+    }
+    return jsonify(item)
---
+++ static/js/comments.js
@@ -0,0 +1,62 @@
+var options = {
+  //Boolean - Whether to show lines for each scale point
+  scaleShowLine : true,
+  //Boolean - Whether we show the angle lines out of the radar
+  angleShowLineOut : true,
+  //Boolean - Whether to show labels on the scale
+  scaleShowLabels : false,
+  // Boolean - Whether the scale should begin at zero
+  scaleBeginAtZero : true,
+  //String - Colour of the angle line
+  angleLineColor : "rgba(0,0,0,.1)",
+  //Number - Pixel width of the angle line
+  angleLineWidth : 1,
+  //String - Point label font declaration
+  pointLabelFontFamily : "'Arial'",
+  //String - Point label font weight
+  pointLabelFontStyle : "normal",
+  //Number - Point label font size in pixels
+  pointLabelFontSize : 12,
+  //String - Point label font colour
+  pointLabelFontColor : "#555",
+  //Boolean - Whether to show a dot for each point
+  pointDot : true,
+  //Number - Radius of each point dot in pixels
+  pointDotRadius : 3,
+  //Number - Pixel width of point dot stroke
+  pointDotStrokeWidth : 1,
+  //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+  pointHitDetectionRadius : 20,
+  //Boolean - Whether to show a stroke for datasets
+  datasetStroke : true,
+  //Number - Pixel width of dataset stroke
+  datasetStrokeWidth : 2,
+  //Boolean - Whether to fill the dataset with a colour
+  datasetFill : true,
+  //String - A legend template
+  // legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+};
+$(document).ready(function(){
+  var store = document.getElementById('store');
+  var event_id = store.dataset.event_id;
+  var presenter = store.dataset.presenter;
+  $.get('/comment/api/' + event_id + '/' + presenter, function(rating) {
+    var data = {
+      labels: rating.labels,
+      datasets: [
+        {
+          label: "Reco Study Rating",
+          fillColor: "rgba(120,220,220,0.2)",
+          strokeColor: "rgba(100,220,220,1)",
+          pointColor: "rgba(200,220,220,1)",
+          pointStrokeColor: "#aff",
+          pointHighlightFill: "#aff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: rating.data
+        }
+      ]
+    };
+    var ctx = document.getElementById("ratingChart").getContext("2d");
+    var myRadarChart = new Chart(ctx).Radar(data, options);
+  });
+});
