96f74ff140362bf7197f52b4d838dc730e89077d
y-matsuki <yuto.matsuki@recochoku.co.jp>
2015-09-16 16:51:04 +0900

    bug fix

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+sudo yum install -y git
+echo "[MongoDB]
+name=MongoDB Repository
+baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64
+gpgcheck=0
+enabled=1" | sudo tee -a /etc/yum.repos.d/mongodb.repo
+sudo yum install -y mongodb-org-server mongodb-org-shell mongodb-org-tools
+sudo service mongod start
+
+sudo pip install flask
+sudo pip install pymongo
+sudo pip install passlib
+
+python init_mongodb.py
